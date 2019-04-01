082194af959b68d7bfce34f2346098da9258bf57
Ming <a@ufoym.com>
2019-04-01 21:51:14 +0800

    Add CUDA90 branch

--- generator/modules/tensorflow.py
+++ generator/modules/tensorflow.py
@@ -9,11 +9,22 @@ class Tensorflow(Module):
 
     def build(self):
         is_gpu = '' if self.composer.cuda_ver is None else '-gpu'
-        return r'''
-            $PIP_INSTALL \
-                tf-nightly%s-2.0-preview \
-                && \
-        ''' % is_gpu
+        if self.composer.cuda_ver == '10.0':
+            return r'''
+                $PIP_INSTALL \
+                    tf-nightly%s-2.0-preview \
+                    && \
+            ''' % is_gpu
+        else:
+            tf_version = self.composer.ver(Tensorflow)
+            tf_version = '1.12' if self.composer.cuda_ver == '9.0' else '1.4'
+            tf_version = '' if 'latest' == tf_version else '==' + tf_version
+            return r'''
+                $PIP_INSTALL \
+                    tensorflow%s%s \
+                    && \
+            ''' % (is_gpu, tf_version)
+
 
     def expose(self):
         return [
